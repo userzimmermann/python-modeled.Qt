fa568283bf09168d2880cff1f9de6f80e20aa9e7
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-05-14 06:59:54 +0000

    setup. VERSION: 0.0.1

---
+++ setup.py
@@ -0,0 +1,48 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+VERSION = open('VERSION').read().strip()
+
+REQUIRES = open('requirements.txt').read()
+
+
+setup(
+  name='modeled.Qt',
+  version=VERSION,
+  description="Qt GUI Adapter for modeled objects.",
+
+  author='Stefan Zimmermann',
+  author_email='zimmermann.code@gmail.com',
+  url='http://bitbucket.org/userzimmermann/python-modeled',
+
+  license='LGPLv3',
+
+  install_requires=REQUIRES,
+
+  namespace_packages=['modeled'],
+  py_modules=[
+    'modeled.Qt',
+    ],
+
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved ::'
+    ' GNU Library or Lesser General Public License (LGPL)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development',
+    'Topic :: Utilities',
+    ],
+  keywords=[
+    'modeled', 'model', 'modeling', 'modelled', 'modelling',
+    'serialization', 'exchange', 'mapping',
+    'class', 'object', 'member', 'property', 'typed',
+    'ctypes', 'cfunc', 'funcptr',
+    'python3',
+    ],
+  )
